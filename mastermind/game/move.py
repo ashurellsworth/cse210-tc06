06e0c4e0057eccaedf24f71b8983bd501a0d665b
MichaelMcFarland98 <michaelmcfarland1998@gmail.com>
2021-05-28 19:42:53 -0600

    updated a few classes and worked on the director

--- mastermind/game/move.py
+++ mastermind/game/move.py
@@ -1,5 +1,10 @@
 class Move:
-    """Comment"""
+    """Keep track of the guesses"""
 
-    def __init__(self):
+    def __init__(self, attempt):
         """Comment"""
+        self._attempt = attempt
+
+    def get_attempt(self):
+        """Returns the guess"""
+        return self._attempt
