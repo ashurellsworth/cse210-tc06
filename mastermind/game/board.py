5b139de3794ad5c092fb62e9651e968d5c6540d4
MichaelMcFarland98 <michaelmcfarland1998@gmail.com>
2021-05-29 12:06:23 -0600

    updated director and board

--- mastermind/game/board.py
+++ mastermind/game/board.py
@@ -7,12 +7,20 @@ class Board:
 
     def __init__(self):
         """Comment"""
+        self._number = []
+        self._prepare()
 
     def to_string(self, names):
         """Sets up the board"""
 
         text = "\n--------------------"
-        for name, names in enumerate(names):
+        for name, names in enumerate(self._names):
             text += (f"\n{name}: ")
         text += "\n--------------------"
         return text
+
+    def _prepare(self):
+        """sets up code"""
+        number = random.randiant(1000, 9999)
+        for n in range(number):
+            self._number.append(code)
--- mastermind/game/player.py
+++ mastermind/game/player.py
@@ -11,24 +11,16 @@ class Player:
         """Returns the player's last move (an instance of Move). If the player 
         hasn't moved yet this method returns None.
 
-        Args:
-            self (Player): an instance of Player.
         """
         return self._move
+        
 
     def get_name(self):
         """Returns the player's name.
-
-        Args:
-            self (Player): an instance of Player.
         """
         return self._name
 
     def set_move(self, move):
-        """Sets the player's last move to the given instance of Move.
+        """Sets the player's last move to the given instance of Move."""
 
-        Args:
-            self (Player): an instance of Player.
-            move (Move): an instance of Move
-        """
         self._move = move
